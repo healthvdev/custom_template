cf98fbc0b5bafddb5c1df1fff24eb0b2a1fb3b05
Soumen Sarkar <soumenc3@gmail.com>
2016-07-16 22:57:32 +0530

    Added title

--- citypage.php
+++ citypage.php
@@ -1,43 +1,20 @@
 <?php /* Template Name: CityPage */ ?>
  
-<?php get_header(); ?>
- 
-<div id="primary" class="content-area">
-    <main id="main" class="site-main" role="main">
-
-<div class="widget optimizer_front_about aboutblock about_inner" style="padding:40px 0px;float: none; ">
-
-        <div class="about_content">
-            <p>
-                <img class="alignnone size-large wp-image-185" src="http://healthvings.com/wp-content/uploads/2016/07/boy-1.png" alt="boy-1" width="48" height="48"> &nbsp;
-                <img class="alignnone size-large wp-image-188" src="http://healthvings.com/wp-content/uploads/2016/07/girl-1.png" alt="girl-1" width="48" height="48"> &nbsp;
-                <img class="alignnone size-large wp-image-190" src="http://healthvings.com/wp-content/uploads/2016/07/girl-3.png" alt="girl-3" width="48" height="48"> 
-                <img class="alignnone size-large wp-image-192" src="http://healthvings.com/wp-content/uploads/2016/07/girl.png" alt="girl" width="48" height="48"> &nbsp;
-                <img class="alignnone size-large wp-image-194" src="http://healthvings.com/wp-content/uploads/2016/07/man.png" alt="man" width="48" height="48"> 
-                <img class="alignnone size-large wp-image-195" src="http://healthvings.com/wp-content/uploads/2016/07/people.png" alt="people" width="48" height="48"> 
-                <img class="alignnone size-full wp-image-197" src="http://healthvings.com/wp-content/uploads/2016/07/writer.png" alt="writer" width="48" height="48">
-            </p>
-            <span class="about_pre" style="font-size: 20px;">Join India's biggest support community Diabetics</span>
-            <div class="cta_buttons" style="margin-top:10px;">
-<?php if(!is_user_logged_in()){?>
-            <a class="static_cta2 lts_button lt_rounded cta_flat" style="padding: 2px 25px;" href="/register">JOIN NOW</a>
-<?php }else { ?>
-            <a class="static_cta2 lts_button lt_rounded cta_flat" style="padding: 2px 25px;" href="/community">EXPLORE COMMUNITY</a>
-<?php } ?>
-            </div>
-        </div>
-    </div>
+<?php 
+?>
 
 
-
- <?php       
-
+<?php 
+/*
+Custom Code for reading API
+*/
 function getURLParams($new_network, $new_state, $new_city, $new_count, $new_page){
     return "?network=".$new_network."&"."state=".$new_state."&"."city=".$new_city."&"."pageCount=".$new_count."&"."pageNumber=".$new_page;
 }
 
 
-$path    = "/var/www/html/wp-content/themes/optimizer";
+
+$path    = "/var/www/html/wordpress/wordpress/wp-content/themes/optimizer";
 require_once 'template/Twig/lib/Twig/Autoloader.php';
 require_once 'pest/PestJSON.php';
 
@@ -87,13 +64,58 @@ if($total_count<10){
     $rounded_count = round($total_count, -3).'+';
 }
 
-echo $twig->render('hospital_list.html', 
-    array('response' => $mapData,
-        'title' => $rounded_count. ' Hospitals in '
+$title = $rounded_count. ' Hospitals in '
         . ($network=='all'?' ': $network.' Network, ')
         .($city=='all'?'': $city.', ')
         .($state=='all'?'': $state.',')
-        .' India',
+        .' India';
+
+?>
+
+
+
+<?php
+
+global $title;
+echo $title . "=====" . wp_title($title).'+++';
+
+get_header(); 
+?>
+ 
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+
+<div class="widget optimizer_front_about aboutblock about_inner" style="padding:40px 0px;float: none; ">
+
+        <div class="about_content">
+            <p>
+                <img class="alignnone size-large wp-image-185" src="http://healthvings.com/wp-content/uploads/2016/07/boy-1.png" alt="boy-1" width="48" height="48"> &nbsp;
+                <img class="alignnone size-large wp-image-188" src="http://healthvings.com/wp-content/uploads/2016/07/girl-1.png" alt="girl-1" width="48" height="48"> &nbsp;
+                <img class="alignnone size-large wp-image-190" src="http://healthvings.com/wp-content/uploads/2016/07/girl-3.png" alt="girl-3" width="48" height="48"> 
+                <img class="alignnone size-large wp-image-192" src="http://healthvings.com/wp-content/uploads/2016/07/girl.png" alt="girl" width="48" height="48"> &nbsp;
+                <img class="alignnone size-large wp-image-194" src="http://healthvings.com/wp-content/uploads/2016/07/man.png" alt="man" width="48" height="48"> 
+                <img class="alignnone size-large wp-image-195" src="http://healthvings.com/wp-content/uploads/2016/07/people.png" alt="people" width="48" height="48"> 
+                <img class="alignnone size-full wp-image-197" src="http://healthvings.com/wp-content/uploads/2016/07/writer.png" alt="writer" width="48" height="48">
+            </p>
+            <span class="about_pre" style="font-size: 20px;">Join India's biggest support community Diabetics</span>
+            <div class="cta_buttons" style="margin-top:10px;">
+<?php if(!is_user_logged_in()){?>
+            <a class="static_cta2 lts_button lt_rounded cta_flat" style="padding: 2px 25px;" href="/register">JOIN NOW</a>
+<?php }else { ?>
+            <a class="static_cta2 lts_button lt_rounded cta_flat" style="padding: 2px 25px;" href="/community">EXPLORE COMMUNITY</a>
+<?php } ?>
+            </div>
+        </div>
+    </div>
+
+
+
+ <?php       
+
+
+echo $twig->render('hospital_list.html', 
+    array('response' => $mapData,
+        'title' => $title,
               'params' => array('state' => rawurlencode($state),
                             'city' => rawurlencode($city),
                             'network' => rawurlencode($network),
