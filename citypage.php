e5d6890ebb286ca8324d8b843410d09530243d7e
Soumen Sarkar <soumenc3@gmail.com>
2017-02-01 16:58:05 +0530

    english hint in title

--- citypage.php
+++ citypage.php
@@ -180,9 +180,10 @@ if($locale_code == "hi_IN"){
 	        . ($specialty=='all'?($chain=='all'?' ': ' '.__($chain,"optimizer")): ' '.__($specialty,"optimizer"). ' के विशेष ')
 			.' अस्पताल ('
 			. ($specialty=='all'?($chain=='all'?' ': ' '.__($chain,"optimizer")): ' '.__($specialty,"optimizer"))
-			. ' Hospitals in '
-	        . ($network=='all'?' ': __($network,"optimizer").' Network, ')
-	        .($city=='all'?'': __($city,"optimizer").' in  Hindi')
+			. '    ('
+	        . ($network=='all'?' ': $network.' Network, ')
+	        . ($city=='all'?'': $city)
+	        . ' in  Hindi)'
 			;
 
 
